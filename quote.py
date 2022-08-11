297c5565a1dfbbe7c1ac7bfca0a0aaff322a12ec
Raven <raven@haoslabs.io>
2022-08-12 01:33:04 +0300

    Fix images

--- quote.py
+++ quote.py
@@ -21,33 +21,31 @@ def write_to_pic(text, tagged):
 
     S, W = 80, 640
 
-    draw.rectangle((S, S, W, W))
+    current_h, pad = S, 10
 
-    current_h, pad = S + 10, 10
+    w, h = draw.textsize(para[0], font=font)
 
-    current_h += current_h * (11 - len(para)) / 3
+    current_h += h * ((12 - len(para)) / 2)
 
-    for line in para:
-        w, h = draw.textlength(line, font=font)
+    draw.text(((W - w + S) / 2, current_h + S / 2), para[0], font=font)
+    current_h += h + pad
+
+    for line in para[1:]:
+        w, h = draw.textsize(line, font=font)
         draw.text(((W - w + S) / 2, current_h + S / 2), line, font=font)
         current_h += h + pad
 
     w, h = draw.textsize(tagged, font=font)
-    draw.text(((W - w + S) / 2, current_h + h + S / 2), tagged, font=font)
-    image.save("quoteBgs/test.png")
-
-
-img = get_random_img()
-write_to_pic(
-    "If you're visiting this page, you're likely here because you're searching for a random sentence. Sometimes a random word just isn't enough, and that is where the random sentence generator comes into play. By inputting the desir ",
-    "@ravenFTX")
+    draw.text(((W - w + S) / 2, current_h + 1.5 * h + S / 2), tagged, font=font)
+    # image.save("quoteBgs/test.png")
+    return image
 
 # resize
-exit(0)
-for image_path in os.listdir("bgs"):
-    print(image_path)
-    img = Image.open(f'bgs/{image_path}')
-    wsize = 1280
-    hsize = 720
-    img = img.resize((wsize, hsize), Image.LANCZOS)
-    img.save(f'quoteBgs/{image_path}')
+# exit(0)
+# for image_path in os.listdir("bgs"):
+#     print(image_path)
+#     img = Image.open(f'bgs/{image_path}')
+#     wsize = 1280
+#     hsize = 720
+#     img = img.resize((wsize, hsize), Image.LANCZOS)
+#     img.save(f'quoteBgs/{image_path}')
