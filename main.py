8188bdec0a7dc1dd46e8f427e2d9ad9c73870483
Raven <raven@haoslabs.io>
2022-08-12 02:13:35 +0300

    Add pillow

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ for x in jsonLang:
 def translate(sentence):
     linksRemoved = [
         re.sub(
-            "(@[A-Za-z0-9]+)|([^0-9A-Za-z \n\'])|(\w+:\/\/\S+)|(www.\S+)",
+            "(@[A-Za-z0-9]+)|([^0-9A-Za-z #\n\'])|(\w+:\/\/\S+)|(www.\S+)",
             '',
             x).lower() for x in sentence.split(sep=" ")]
     return " ".join(
